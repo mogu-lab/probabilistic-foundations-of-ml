58a25805044da665e2a23fd0a1e665f41aea44f8
Yaniv Yacoby <yanivyacoby@gmail.com>
2024-04-10 17:59:32 -0400

    updated

--- materials/data.py
+++ materials/data.py
@@ -1,15 +1,31 @@
+from collections import defaultdict
+
 import pandas as pd
 import jax.numpy as jnp
 import jax.random as jrandom
 import numpyro
 import numpyro.distributions as D
+import numpyro.handlers as H
+import chex
+
+
+IDX_TO_DAY_OF_WEEK = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+]
 
 
 IDX_TO_CONDITION = [
+    'High Fever',    
     'Broken Limb',    
-    'High Fever',
     'Entangled Antennas',
     'Allergic Reaction',
+    'Intoxication',
 ]
 
 IDX_TO_BOOL = [
@@ -18,53 +34,106 @@ IDX_TO_BOOL = [
 ]
 
 
-def generate_data_for_q1():
-    key = jrandom.PRNGKey(seed=0)
-    key_weekday, key_weekend, key_condition, key_hospitalized = jrandom.split(key, 4)
-    
-    NUM_DAYS = 200
+def model_discrete_IHH_ER(N, data=None):
+    assert(N > 0)
+    if data is None:
+        data = {
+            'Day-of-Week': None,
+            'Condition': None,
+            'Hospitalized': None,
+            'Antibiotics': None,
+        }
 
-    day = jnp.arange(NUM_DAYS)
     
-    p_visits_weekday = D.Poisson(rate=50.0)
-    p_visits_weekend = D.Poisson(rate=25.0)
-
-    num_visits = jnp.where(
-        (day % 7 == 5) | (day % 7 == 6),
-        p_visits_weekend.sample(key_weekend, (NUM_DAYS,)),
-        p_visits_weekday.sample(key_weekday, (NUM_DAYS,)),
+    pi_day_of_week = numpyro.param(
+        'pi_day_of_week',
+        jnp.array([0.20, 0.15, 0.14, 0.14, 0.14, 0.11, 0.12]),
     )
 
-    p_condition = D.Categorical(jnp.array([0.2, 0.4, 0.1, 0.3]))
-    p_hospitalized = D.Bernoulli(jnp.array([0.2, 0.1, 0.1, 0.7]))
+    pi_condition_given_is_weekend = numpyro.param(
+        'pi_condition_given_is_weekend',
+        jnp.array([
+            [0.3, 0.1, 0.1, 0.4, 0.1],
+            [0.2, 0.06666667, 0.06666667, 0.26666667, 0.4],
+        ]),
+    )
+
+    pi_hospitalized_given_condition = numpyro.param(
+        'pi_hospitalized_given_condition',
+        jnp.array([0.7, 0.1, 0.1, 0.5, 0.0]),
+    )
+
+    pi_antibiotics_given_is_allergy_and_hospitalized = numpyro.param(
+        'pi_antibiotics_given_condition_and_hospitalized',
+        jnp.array([0.0, 0.8]),
+    )
     
-    conditions = p_condition.sample(key_condition, (int(num_visits.sum()),))
-    hospitalized = p_hospitalized.sample(key_condition, (int(num_visits.sum()),))    
-    hospitalized = hospitalized[(jnp.arange(conditions.size), conditions)]
+    with numpyro.plate('data', N):
+        p_day_of_week = D.Categorical(pi_day_of_week)                    
+        day_of_week = numpyro.sample(
+            'Day-of-Week', p_day_of_week, obs=data['Day-of-Week'],
+        )
+        chex.assert_shape(day_of_week, (N,))
 
-    rows = []
-    idx = 0
-    for day, num_visits in zip(day, num_visits):
-        for _ in range(num_visits):
-            rows.append({
-                'Day': day,
-                'Condition': IDX_TO_CONDITION[conditions[idx]],
-                'Hospitalized': IDX_TO_BOOL[hospitalized[idx]],
-            })
+        is_weekend = ((day_of_week % 7 == 5) | (day_of_week % 7 == 6)).astype('int32')
+        p_condition_given_is_weekend = D.Categorical(
+            pi_condition_given_is_weekend[is_weekend],
+        )
+        condition = numpyro.sample(
+            'Condition', p_condition_given_is_weekend, obs=data['Condition'],
+        )
+        chex.assert_shape(condition, (N,))
+        
+        p_hospitalized_given_condition = D.Bernoulli(
+            pi_hospitalized_given_condition[condition],
+        )
+        hospitalized = numpyro.sample(
+            'Hospitalized', p_hospitalized_given_condition, obs=data['Hospitalized'],
+        )
+        chex.assert_shape(hospitalized, (N,))
 
-            idx += 1
+        allergy_and_hospitalized = ((condition == 3) & hospitalized).astype('int32')
+        p_antibiotics = D.Bernoulli(
+            pi_antibiotics_given_is_allergy_and_hospitalized[allergy_and_hospitalized]
+        )
+        antibiotics = numpyro.sample(
+            'Antibiotics', p_antibiotics, obs=data['Antibiotics'],
+        )
+        chex.assert_shape(antibiotics, (N,))
 
-    df = pd.DataFrame(rows)
-    df.index.name = 'Patient ID'
 
-    return df
+def jax_int_array_to_str_list(a, idx_to_s):
+    return [idx_to_s[i] for i in a.tolist()]
 
         
 def main():
-    df = generate_data_for_q1()
+    N = 10000
+    with H.seed(rng_seed=0):
+        exec_trace = H.trace(model_discrete_IHH_ER).get_trace(N)
+
+    df = pd.DataFrame({
+        'Day-of-Week': jax_int_array_to_str_list(
+            exec_trace['Day-of-Week']['value'],
+            IDX_TO_DAY_OF_WEEK,
+        ),
+        'Condition': jax_int_array_to_str_list(
+            exec_trace['Condition']['value'],
+            IDX_TO_CONDITION,
+        ),
+        'Hospitalized': jax_int_array_to_str_list(
+            exec_trace['Hospitalized']['value'],
+            IDX_TO_BOOL,
+        ),
+        'Antibiotics': jax_int_array_to_str_list(
+            exec_trace['Antibiotics']['value'],
+            IDX_TO_BOOL,
+        ),
+    })
+
+    df.index.name = 'Patient ID'
     df.to_csv('IHH-ER.csv')
 
-
+        
 if __name__ == '__main__':
     main()
 
