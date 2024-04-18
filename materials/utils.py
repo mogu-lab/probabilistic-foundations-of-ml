a8debdc08778a73b730aa432634c4dc91fb1a938
Yaniv Yacoby <yanivyacoby@gmail.com>
2024-04-18 12:07:30 -0400

    updated

--- materials/utils.py
+++ materials/utils.py
@@ -4,9 +4,12 @@ from matplotlib_venn import venn2
 
 
 def display_joint_probability_illustration():
+    fig, ax = plt.subplots(1, 1, figsize=(2, 4))
+    
     out = venn2(
         subsets=(2, 2, 1),
         set_labels=('$A = a$', '$B = b$', '$A = a, B = b$'),
+        ax=ax,
     )
     
     for idx, subset in enumerate(out.subset_labels):
