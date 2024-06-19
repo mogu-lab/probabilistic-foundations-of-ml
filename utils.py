5b1939fcf0101162efabea8f474e5ad79ee5d800
Yaniv Yacoby <yanivyacoby@gmail.com>
2024-06-19 15:09:04 -0400

    updated

--- utils.py
+++ utils.py
@@ -1,8 +1,24 @@
+import argparse
+
 import matplotlib
 import matplotlib.pyplot as plt; plt.rcParams['figure.dpi'] = 200
 import jax
 import jax.numpy as jnp
 import numpyro.handlers as H
+
+
+def convert_categorical_to_int(d, categories):
+    r = 0
+    for idx, day in enumerate(categories):
+        r += (d == day) * idx
+
+    return r
+
+
+def convert_day_of_week_to_int(d):
+    return convert_categorical_to_int(
+        d, ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    )
 
 
 def plot_poisson_example():
