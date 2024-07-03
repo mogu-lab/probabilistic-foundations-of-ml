9ecd866e864641d0291f4f3ffc463a321f6fc4ee
Yaniv Yacoby <yanivyacoby@gmail.com>
2024-07-03 15:17:06 -0400

    updated

--- scripts/deepnote_init.py
+++ scripts/deepnote_init.py
@@ -1,11 +1,20 @@
+###############################################################################
+# CS349 DeepNote Setup. DO NOT MODIFY.
+###############################################################################
+
+
 import os
 import requests
+
 
 USER_NAME = 'mogu-lab'
 REPO_NAME = 'cs349-fall-2024'
 
 
 def download_directory_from_github(user_name, repo_name, path, skip_downloaded=True):
+    '''
+    Downloads all files in a directory from a github repository.
+    '''
     url = f'https://api.github.com/repos/{user_name}/{repo_name}/contents/{path}'
     directory_info = requests.get(url)
     
@@ -19,6 +28,10 @@ def download_directory_from_github(user_name, repo_name, path, skip_downloaded=T
 
 
 def download_files_from_github(user_name, repo_name, paths, skip_downloaded=True):
+    '''
+    Downloads a list of files from a github repository.
+    '''
+    
     for path in paths:
         if os.path.exists(path) and skip_downloaded:
             continue
@@ -34,9 +47,11 @@ def download_files_from_github(user_name, repo_name, paths, skip_downloaded=True
 
 # Load data
 print('Loading Data:')
-
 os.makedirs('data', exist_ok=True)
 download_directory_from_github(USER_NAME, REPO_NAME, 'data')
+
+# Load helper Python files
+print('Loading helper Python files:')
 download_files_from_github(USER_NAME, REPO_NAME, ['cs349.py', 'utils.py'])
 
 print('Done.')
